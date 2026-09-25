a38afd737ab98eb3111dce020be7ce1299c6a151
gustavoLimaAraujo <gustavo.araujo@sptech.school>
2026-09-25 09:14:59 -0300

    feat: adicionado estrutura e estilização do pop-up

---
+++ public/js/editar-usuario.js
@@ -0,0 +1,11 @@
+const popUpEditarUsuario = `
+
+`
+
+/*
+O que eu pretendo fazer neste arquivo JS:
+    - Colocar toda a estrutura html no JS
+    - Fazer que quando clicar no usuário do aside, aparecer esse pop-up
+    - Fazer com que quando clicar fora do pop-up, feixe o pop-up automaticamente
+    - Verificar se é possível, mudar a opacitdade da tela atrás do pop-up para deixar mais escuro
+*/
