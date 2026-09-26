5704fbdefec402dc21d89dd5da62690907a1c1b9
gustavoLimaAraujo <gustavo.araujo@sptech.school>
2026-09-25 22:03:26 -0300

    feat: lógica e interatividade com o pop-up

--- public/js/editar-usuario.js
+++ public/js/editar-usuario.js
@@ -1,11 +1,79 @@
+let localizacaoPopUpEditarUsuario = document.getElementById("editar-usuario");
+let localizacaoContainerMain = document.getElementById("container_main");
+
 const popUpEditarUsuario = `
+    <div class="popup-editar">
+        <p class="fechar" onclick="fecharEditarUsuario()"> 
+            X
+        </p>
+        <h3 class="titulo">
+            Atualize as suas informações
+        </h3>
 
+        <form>
+            <div class="input-form">
+                <label for="ipt_nome_completo">
+                    Nome Completo
+                </label>
+                <input 
+                type="text" 
+                id="ipt_nome_completo"
+                value="Ana Souza">
+            </div>
+
+            <div class="input-form">
+                <label for="ipt_email_corporativo">
+                    E-mail Corporativo
+                </label>
+                <input 
+                type="email" 
+                id="ipt_email_corporativo"
+                value="ana@empresa.com.br">
+            </div>
+
+            <div class="input-form">
+                <label for="ipt_redefinir_senha">
+                    Redefinir Senha
+                </label>
+                <input 
+                type="password" 
+                id="ipt_redefinir_senha"
+                value="">
+            </div>
+
+            <div class="input-form">
+                <label for="ipt_confirmar_senha">
+                    Confirmar Senha
+                </label>
+                <input 
+                type="password" 
+                id="ipt_confirmar_senha"
+                value="">
+            </div>
+        </form>
+
+        <div class="sessao-butoes">
+            <button class="botao atualizar-conta">
+                Atualizar Informações
+            </button>
+
+            <button class="botao deletar-conta">
+                Deletar Conta
+            </button>
+        </div>
+
+    </div>
 `
+localizacaoPopUpEditarUsuario.innerHTML = popUpEditarUsuario;
 
-/*
-O que eu pretendo fazer neste arquivo JS:
-    - Colocar toda a estrutura html no JS
-    - Fazer que quando clicar no usuário do aside, aparecer esse pop-up
-    - Fazer com que quando clicar fora do pop-up, feixe o pop-up automaticamente
-    - Verificar se é possível, mudar a opacitdade da tela atrás do pop-up para deixar mais escuro
-*/
+function abrirEditarUsuario() {
+    localizacaoPopUpEditarUsuario.style.display = "block";
+    localizacaoContainerMain.style.filter = "brightness(0.5)";
+    localizacaoContainerMain.style.background = "rgba(221, 221, 221, 0.9)";
+}
+
+function fecharEditarUsuario() {
+    localizacaoPopUpEditarUsuario.style.display = "none";
+    localizacaoContainerMain.style.filter = "none";
+    localizacaoContainerMain.style.background = "none";
+}
